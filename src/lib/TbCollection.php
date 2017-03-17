2db11c00887081279e9f6b90e765c3f88784785a
dzer <358654744@qq.com>
2017-03-17 11:40:57 +0800

    Revert "修改获取图片后缀错误bug"
    This reverts commit 90fb8f2a8a7bba8cc84c8d907feaecd7d15c5a4c.
    
    # Conflicts:
    #	src/lib/TbCollection.php

--- src/lib/TbCollection.php
+++ src/lib/TbCollection.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace dzer\coltaobao\lib;
 
 use dzer\coltaobao\basic\Request;
@@ -10,7 +9,6 @@ use Exception;
  * 采集店铺所有商品
  *
  * @author dzer <d20053140@gmail.com>
- *
  * @version 2.0
  */
 class TbCollection extends TbBase
@@ -31,10 +29,11 @@ class TbCollection extends TbBase
                 $this->shopType = 2;
             }
         }
+
     }
 
     /**
-     * 采集主要流程.
+     * 采集主要流程
      */
     public function colMain()
     {
@@ -46,15 +45,14 @@ class TbCollection extends TbBase
         $this->colGoodsInfo();
 
         $end_time = $this->microTime();
-        $this->log->info('店铺：'.$this->shopId.',共采集商品：'.count($this->goodsList).'条，用时：'.($end_time - $start_time)."秒\r\n");
+        $this->log->info("店铺：" . $this->shopId . ',共采集商品：' . count($this->goodsList) . '条，用时：' . ($end_time - $start_time) ."秒\r\n");
     }
 
     /**
-     * 采集商品基本信息列表.
-     *
-     * @throws Exception
+     * 采集商品基本信息列表
      *
      * @return array
+     * @throws Exception
      */
     protected function colGoodsList()
     {
@@ -75,11 +73,10 @@ class TbCollection extends TbBase
     }
 
     /**
-     * 获取总页数.
-     *
-     * @throws Exception
+     * 获取总页数
      *
      * @return mixed
+     * @throws Exception
      */
     public function getGoodsPage()
     {
@@ -99,29 +96,28 @@ class TbCollection extends TbBase
         if (isset($page[1])) {
             $page_num = explode('/', $page[1]);
         }
-
         return isset($page_num[1]) ? $page_num[1] : false;
     }
 
     /**
-     * 获取商品页面url.
+     * 获取商品页面url
      *
      * @param int $pageNum 总页数
-     *
      * @return array
      */
     public function getGoodsPageUrl($pageNum)
     {
-        $url = [];
+        $url = array();
         for ($i = 1; $i <= $pageNum; $i++) {
-            $url[] = $this->shopUrl.$this->allGoodsUrl.'&pageNum='.$i;
+            $url[] = $this->shopUrl . $this->allGoodsUrl . '&pageNum=' . $i;
         }
-
         return $url;
     }
 
+
     /**
-     * 循环采集商品详细信息.
+     * 循环采集商品详细信息
+     *
      */
     protected function colGoodsInfo()
     {
@@ -144,7 +140,7 @@ class TbCollection extends TbBase
                     } else {
                         $msg = isset($rs->ret[0]) ? $rs->ret[0] : '';
                         $msg .= isset($rs->data->redirectUrl) ? $rs->data->redirectUrl : '';
-                        $this->log->error('采集失败！'.$msg);
+                        $this->log->error('采集失败！' . $msg);
                     }
                 }
             }
@@ -152,31 +148,28 @@ class TbCollection extends TbBase
     }
 
     /**
-     * 获取商品信息页面url列表.
-     *
-     * @throws Exception
+     * 获取商品信息页面url列表
      *
      * @return array
+     * @throws Exception
      */
     public function getGoodsInfoUrl()
     {
-        $url = [];
+        $url = array();
         if (!empty($this->goodsList)) {
             foreach ($this->goodsList as $_goods) {
-                $url[] = $this->goodsInfoUrl.$_goods['id'];
+                $url[] = $this->goodsInfoUrl . $_goods['id'];
             }
         } else {
             throw new Exception('商品列表为空');
         }
-
         return $url;
     }
 
     /**
-     * 保存店铺信息到数据库.
+     * 保存店铺信息到数据库
      *
      * @param $data
-     *
      * @return mixed
      */
     private function saveShop($data)
@@ -188,76 +181,70 @@ class TbCollection extends TbBase
             $img_rs = $request->curl($data->picUrl);
             $data->img = $this->saveImages($img_rs, $data->picUrl, $savePath);
         }
-        $param = [
-            'userNumId'          => $data->userNumId,
-            'type'               => $data->type,
-            'nick'               => $data->nick,
-            'creditLevel'        => $data->creditLevel,
+        $param = array(
+            'userNumId' => $data->userNumId,
+            'type' => $data->type,
+            'nick' => $data->nick,
+            'creditLevel' => $data->creditLevel,
             'goodRatePercentage' => $data->goodRatePercentage,
-            'shopTitle'          => $data->shopTitle,
-            'shopId'             => $data->shopId,
-            'weitaoId'           => $data->weitaoId,
-            'fansCount'          => $data->fansCount,
-            'img'                => $data->img,
-            'picUrl'             => $data->picUrl,
-            'starts'             => $data->starts,
-            'createTime'         => date('Y-m-d H:i:s'),
-        ];
+            'shopTitle' => $data->shopTitle,
+            'shopId' => $data->shopId,
+            'weitaoId' => $data->weitaoId,
+            'fansCount' => $data->fansCount,
+            'img' => $data->img,
+            'picUrl' => $data->picUrl,
+            'starts' => $data->starts,
+            'createTime' => date('Y-m-d H:i:s'),
+        );
         $this->db->where('shopId', $data->shopId)->delete('shop');
         $id = $this->db->insert('shop', $param);
 
         if ($id > 0) {
-            $this->log->info($data->nick.' 店铺信息保存成功！');
-
+            $this->log->info($data->nick . " 店铺信息保存成功！");
             return true;
         } else {
-            $this->log->warning(print_r($data, true)."\r\n店铺保存失败！".$this->db->getLastError());
-
+            $this->log->warning(print_r($data, true) . "\r\n店铺保存失败！" . $this->db->getLastError());
             return false;
         }
     }
 
     /**
-     * 保存商品信息到数据库.
+     * 保存商品信息到数据库
      *
      * @param $data
-     *
      * @return mixed
      */
     private function saveGoods($data)
     {
-        $param = [
-            'shopId'     => $this->shopId,
-            'itemId'     => $data->itemInfoModel->itemId,
-            'title'      => $data->itemInfoModel->title,
-            'favcount'   => $data->itemInfoModel->favcount,
-            'location'   => $data->itemInfoModel->location,
+        $param = array(
+            'shopId' => $this->shopId,
+            'itemId' => $data->itemInfoModel->itemId,
+            'title' => $data->itemInfoModel->title,
+            'favcount' => $data->itemInfoModel->favcount,
+            'location' => $data->itemInfoModel->location,
             'categoryId' => $data->itemInfoModel->categoryId,
-            'price'      => $data->itemInfoModel->price,
+            'price' => $data->itemInfoModel->price,
             'createTime' => date('Y-m-d H:i:s'),
-        ];
+        );
         $this->db->where('itemId', $data->itemInfoModel->itemId)->delete('goods');
         $goodsId = $this->db->insert('goods', $param);
         if ($goodsId > 0) {
-            $this->log->info('商品基本信息保存成功！商品ID：'.$data->itemInfoModel->itemId);
+            $this->log->info("商品基本信息保存成功！商品ID：" . $data->itemInfoModel->itemId);
             //采集banner图片地址并保存
             $this->saveGoodsBanner($data);
             //采集商品描述并保存
             $this->saveGoodsInfo($data);
         } else {
-            $this->log->warning(print_r($param, true)."\r\n商品保存失败！".$this->db->getLastError());
-
+            $this->log->warning(print_r($param, true) . "\r\n商品保存失败！" . $this->db->getLastError());
             return false;
         }
-
         return true;
     }
 
     /**
-     * 保存商品描述和其他信息.
+     * 保存商品描述和其他信息
      *
      * @param $data
-     *
      * @return bool
      */
     private function saveGoodsInfo($data)
@@ -265,36 +252,32 @@ class TbCollection extends TbBase
         $this->db->where('itemId', $data->itemInfoModel->itemId)->delete('goods_info');
         //采集描述
         $fullDesc = $this->colGoodsDesc($data->descInfo->fullDescUrl);
-        $param = [
-            'itemId'     => $data->itemInfoModel->itemId,
-            'skuProps'   => !empty($data->skuModel->skuProps) ? json_encode($data->skuModel->skuProps) : '',
-            'props'      => !empty($data->props) ? json_encode($data->props) : '',
-            'pcDescUrl'  => $data->descInfo->pcDescUrl,
-            'h5DescUrl'  => $data->descInfo->h5DescUrl,
-            'fullDesc'   => $fullDesc,
+        $param = array(
+            'itemId' => $data->itemInfoModel->itemId,
+            'skuProps' => !empty($data->skuModel->skuProps) ? json_encode($data->skuModel->skuProps) : '',
+            'props' => !empty($data->props) ? json_encode($data->props) : '',
+            'pcDescUrl' => $data->descInfo->pcDescUrl,
+            'h5DescUrl' => $data->descInfo->h5DescUrl,
+            'fullDesc' => $fullDesc,
             'createTime' => date('Y-m-d H:i:s'),
-        ];
+        );
         $this->db->where('itemId', $data->itemInfoModel->itemId)->delete('goods_info');
         $goodsId = $this->db->insert('goods_info', $param);
         if ($goodsId > 0) {
-            $this->log->info('商品描述信息保存成功！商品ID：'.$data->itemInfoModel->itemId);
-
+            $this->log->info("商品描述信息保存成功！商品ID：" . $data->itemInfoModel->itemId);
             return true;
         } else {
-            $this->log->warning(print_r($param, true)."\r\n商品信息保存失败！");
-
+            $this->log->warning(print_r($param, true) . "\r\n商品信息保存失败！");
             return false;
         }
     }
 
     /**
-     * 采集商品描述.
+     * 采集商品描述
      *
      * @param string $fullDescUrl 商品描述Url
-     *
-     * @throws Exception
-     *
      * @return mixed|string
+     * @throws Exception
      */
     private function colGoodsDesc($fullDescUrl)
     {
@@ -314,7 +297,7 @@ class TbCollection extends TbBase
 
                     //采集图片
                     $request = new Request();
-                    $request->callback = ['dzer\coltaobao\lib\TbCollection', 'goodsImgCallback'];
+                    $request->callback = array('dzer\coltaobao\lib\TbCollection', 'goodsImgCallback');
                     $content = $request->curlMulti($img[1]);
 
                     foreach ($content as $img) {
@@ -332,15 +315,13 @@ class TbCollection extends TbBase
                 }
             }
         }
-
         return $fullDescHtml;
     }
 
     /**
-     * 保存商品banner.
+     * 保存商品banner
      *
      * @param $data
-     *
      * @return bool
      */
     private function saveGoodsBanner($data)
@@ -349,36 +330,34 @@ class TbCollection extends TbBase
         $savePath = $this->createDir($this->shopId, $this->goodsId, 'banner');
         //采集banner图片地址并保存
         $request = new Request();
-        $request->callback = ['dzer\coltaobao\lib\TbCollection', 'goodsImgCallback'];
+        $request->callback = array('dzer\coltaobao\lib\TbCollection', 'goodsImgCallback');
         $content = $request->curlMulti($data->itemInfoModel->picsPath);
 
         foreach ($content as $img) {
             $imgPath = $this->saveImages($img['img_rs'], $img['img_url'], $savePath);
             if (!empty($imgPath)) {
-                $banner = [
-                    'itemId'     => $data->itemInfoModel->itemId,
-                    'picsPath'   => $img['img_url'],
-                    'path'       => $imgPath,
+                $banner = array(
+                    'itemId' => $data->itemInfoModel->itemId,
+                    'picsPath' => $img['img_url'],
+                    'path' => $imgPath,
                     'createTime' => date('Y-m-d H:i:s'),
-                ];
+                );
                 $banner_id = $this->db->insert('goods_banner', $banner);
                 if (!$banner_id) {
-                    $this->log->warning(print_r($banner, true)."\r\n商品banner信息保存失败！".$this->db->getLastError());
+                    $this->log->warning(print_r($banner, true) . "\r\n商品banner信息保存失败！" . $this->db->getLastError());
                 }
             }
         }
-
         return true;
     }
 
     /**
-     * 保存指定图片到指定路径.
+     * 保存指定图片到指定路径
      *
-     * @param string $result  图片资源
+     * @param  string $result 图片资源
      * @param string $img_url 图片url
-     * @param string $path    图片保存的目录
-     *
-     * @return string
+     * @param string $path 图片保存的目录
+     * @return  string
      */
     protected function saveImages($result, $img_url, $path)
     {
@@ -386,31 +365,29 @@ class TbCollection extends TbBase
             mkdir($path, 0777, true);
         }
         preg_match('/\.jpg|\.png|\.gif|\.bmp|\.jpeg/s', strtolower($img_url), $extension);
-        $extension = isset($extension[0]) ? $extension[0] : '.jpg';
-        $name = time().rand(000, 999).$extension;
-        $fopen = fopen($path.$name, 'a');
+        $extension = isset($extension[0]) ? $extension[0] : 'jpg';
+        $name = time() . rand(000, 999) . '.' . $extension;
+        $fopen = fopen($path . $name, "a");
         @fwrite($fopen, $result);
         @fclose($fopen);
-        if (file_exists($path.$name)) {
-            return $path.$name;
-        } else {
-            return;
-        }
+        if (file_exists($path . $name))
+            return $path . $name;
+        else
+            return null;
     }
 
-    public static function goodsImgCallback($img_rs, $img_url)
+    static function goodsImgCallback($img_rs, $img_url)
     {
-        return [
-            'img_rs'  => $img_rs,
-            'img_url' => $img_url,
-        ];
+        return array(
+            'img_rs' => $img_rs,
+            'img_url' => $img_url
+        );
     }
 
     /**
-     * 正则匹配商品列表.
+     * 正则匹配商品列表
      *
      * @param string $content 采集的html
-     *
      * @return array
      */
     public static function regExGoods(&$content)
@@ -419,7 +396,7 @@ class TbCollection extends TbBase
         $content = iconv('GBK', 'UTF-8//IGNORE', $content);
         //正则匹配出每个li
         preg_match_all('/<li class="item-wrap.+?>(.+?)<\/li>/s', $content, $goodsListLi);
-        $goodsList = [];
+        $goodsList = array();
         if (isset($goodsListLi[1]) && !empty($goodsListLi[1])) {
             foreach ($goodsListLi[1] as $_li) {
                 //正则匹配出每个li里面的 商品id 缩略图 名称 价格
@@ -430,24 +407,23 @@ class TbCollection extends TbBase
                 //匹配价格
                 preg_match('/<p class="price">.+"value">(.+?)<.+<\/p>/s', $_li, $price);
                 if (isset($name[1]) && !empty($name[1])) {
-                    $goodsList[$name[1]] = [
-                        'id'    => isset($name[1]) ? trim($name[1]) : '',
-                        'name'  => isset($name[2]) ? trim($name[2]) : '',
+                    $goodsList[$name[1]] = array(
+                        'id' => isset($name[1]) ? trim($name[1]) : '',
+                        'name' => isset($name[2]) ? trim($name[2]) : '',
                         'price' => isset($price[1]) ? trim($price[1]) : '',
-                    ];
+                    );
                 } else {
                     continue;
                 }
             }
         }
-
         return $goodsList;
     }
 
-    public function microTime()
+
+    function microTime()
     {
         list($u_sec, $sec) = explode(' ', microtime());
-
-        return floatval($u_sec) + floatval($sec);
+        return (floatval($u_sec) + floatval($sec));
     }
 }
